32f45cfde33d2ea768710a7c55eb80911b2044b6
DwIgor <igormv15@gmail.com>
2022-01-14 15:08:19 -0300

    atividade BlogPessoal

---
+++ DERs/BlogP.sql
@@ -0,0 +1,68 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
+USE `mydb` ;
+
+-- -----------------------------------------------------
+-- Table `tb_temas`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `tb_temas` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
+  `descricao` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `tb_usuarios`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `tb_usuarios` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(255) NOT NULL,
+  `usuario` VARCHAR(255) NOT NULL,
+  `senha` VARCHAR(255) NOT NULL,
+  `foto` VARCHAR(255) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `tb_postagens`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `tb_postagens` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
+  `titulo` VARCHAR(100) NOT NULL,
+  `texto` VARCHAR(1000) NOT NULL,
+  `dat_public` DATE NULL,
+  `tb_temas_id` BIGINT NOT NULL,
+  `tb_usuarios_id` BIGINT NOT NULL,
+  PRIMARY KEY (`id`, `tb_temas_id`, `tb_usuarios_id`),
+  INDEX `fk_tb_postagens_tb_temas_idx` (`tb_temas_id` ASC) VISIBLE,
+  INDEX `fk_tb_postagens_tb_usuarios1_idx` (`tb_usuarios_id` ASC) VISIBLE,
+  CONSTRAINT `fk_tb_postagens_tb_temas`
+    FOREIGN KEY (`tb_temas_id`)
+    REFERENCES `tb_temas` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_tb_postagens_tb_usuarios1`
+    FOREIGN KEY (`tb_usuarios_id`)
+    REFERENCES `tb_usuarios` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
